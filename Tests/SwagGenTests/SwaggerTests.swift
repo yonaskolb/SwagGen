c647c9f7b0d72206a626ff5a9087764da23fecf1
Yonas Kolb <yonaskolb@gmail.com>
2017-02-18 12:45:36 +0700

    add first tests

---
+++ Tests/SwagGenTests/SwaggerTests.swift
@@ -0,0 +1,38 @@
+import XCTest
+@testable import SwagGen
+import PathKit
+
+class SwaggerTests: XCTestCase {
+
+    func testSwaggerParsingMinimum() {
+        catchError(message: "Couldn't load spec") {
+            let spec = try SwaggerSpec(path: "Tests/SwagGenTests/Fixtures/swagger_specs/minimum.yml")
+            XCTAssertEqual(spec.operations.count, 1, "spec must have single operation")
+            XCTAssertEqual(spec.operations.first?.path, "/user", "spec must have a /user path")
+        }
+    }
+
+    func testSwaggerParsingPetStore() {
+        catchError(message: "Couldn't load spec") {
+            let spec = try SwaggerSpec(path: "Tests/SwagGenTests/Fixtures/swagger_specs/petstore.yml")
+            XCTAssertEqual(spec.info?.title, "Swagger Petstore", "spec must have title")
+            XCTAssertEqual(spec.operations.count, 3, "spec must have 3 operations")
+            XCTAssertEqual(spec.security.count, 2, "spec must have 2 security definitions")
+            XCTAssertEqual(spec.definitions.count, 2, "spec must have 3 definitions")
+            XCTAssertEqual(spec.tags.count, 1, "spec must have 1 tag")
+            XCTAssertEqual(spec.paths.count, 2, "spec must have 2 endpoints")
+
+            let petDefinition = spec.definitions["Pet"]
+            XCTAssertEqual(petDefinition?.name, "Pet", "spec must have a Pet definition")
+            XCTAssertEqual(petDefinition?.properties[1].name, "name", "spec must have a name property")
+        }
+    }
+
+    func catchError(message: String = "Error:", closure: () throws -> Void) {
+        do {
+            try closure()
+        } catch let error {
+            XCTFail("\(message)\n\(error)")
+        }
+    }
+}

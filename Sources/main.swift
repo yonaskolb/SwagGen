607e0d0d8479e86cfd687e5411da3f4794819b14
Yonas Kolb <yonaskolb@gmail.com>
2017-02-18 10:33:37 +0700

    improved error logging

--- Sources/main.swift
+++ Sources/main.swift
@@ -12,64 +12,92 @@ import Commander
 
 func generate(templatePath: Path, destinationPath: Path, specPath: String, clean: Bool, options: String) {
 
+    guard specPath != "" && URL(string: specPath) != nil else {
+        writeError("Must provide a valid spec")
+        exit(EXIT_FAILURE)
+    }
+
+    let optionsArray = options.components(separatedBy: ",").map{$0.trimmingCharacters(in: .whitespaces)}
+
+    var optionsDictionary: [String: String] = [:]
+    for option in optionsArray {
+        let parts = option.components(separatedBy: ":").map{$0.trimmingCharacters(in: .whitespaces)}
+        if parts.count >= 2 {
+            let key = parts.first!
+            let value = Array(parts.dropFirst()).joined(separator: ":")
+            optionsDictionary[key] = value
+        }
+    }
+
+    let spec:SwaggerSpec
     do {
+        spec = try SwaggerSpec(path: specPath)
+    }
+    catch let error {
+        writeError("Error loading Swagger Spec: \(error)")
+        exit(EXIT_FAILURE)
+    }
+    let specCounts  = getCountString(counts: [
+        "operation": spec.operations.count,
+        "definition": spec.definitions.count,
+        "tag": spec.tags.count,
+        "parameter": spec.parameters.count,
+        ])
+    writeMessage("Loaded spec: \(spec.info?.title != nil ? "\"\(spec.info!.title!)\" - " : "")\(specCounts)")
 
-        guard specPath != "" && URL(string: specPath) != nil else {
-            writeError("Must provide a valid spec")
-            exit(EXIT_FAILURE)
-        }
+    let templateConfig: TemplateConfig
+    do {
+        templateConfig = try TemplateConfig(path: templatePath, options: optionsDictionary)
+    } catch let error {
+        writeError("Error loading template: \(error)")
+        exit(EXIT_FAILURE)
+    }
 
-        let optionsArray = options.components(separatedBy: ",").map{$0.trimmingCharacters(in: .whitespaces)}
+    let templateCounts  = getCountString(counts: [
+        "template file": templateConfig.templateFiles.count,
+        "copied file": templateConfig.copiedFiles.count,
+        "option": templateConfig.options.keys.count,
+        ])
+    writeMessage("Loaded template: \(templateCounts)")
+    if !templateConfig.options.isEmpty {
+        writeMessage("Options:\n\t\(templateConfig.options.map{"\($0): \($1)"}.joined(separator: "\n").replacingOccurrences(of: "\n", with: "\n\t"))")
+    }
+    let codeFormatter: CodeFormatter
+    if let formatter = templateConfig.formatter {
 
-        var optionsDictionary: [String: String] = [:]
-        for option in optionsArray {
-            let parts = option.components(separatedBy: ":").map{$0.trimmingCharacters(in: .whitespaces)}
-            if parts.count >= 2 {
-                let key = parts.first!
-                let value = Array(parts.dropFirst()).joined(separator: ":")
-                optionsDictionary[key] = value
-            }
-        }
-
-        let spec = try SwaggerSpec(path: specPath)
-        writeMessage("Loaded spec: \"\(spec.info.title ?? "")\" - \(spec.operations.count) operations, \(spec.definitions.count) definitions, \(spec.tags.count) tags, \(spec.parameters.count) parameters")
-
-        let templateConfig = try TemplateConfig(path: templatePath, options: optionsDictionary)
-        let counts: [String: Int] = ["template file": templateConfig.templateFiles.count, "copied file": templateConfig.copiedFiles.count, "option": templateConfig.options.keys.count]
-        writeMessage("Loaded template: \(counts.map{"\($0.value) \($0.value == 1 ? $0.key : "\($0.key)s")"}.joined(separator: ", "))")
-        if !templateConfig.options.isEmpty {
-            writeMessage("Options:\n\t\(templateConfig.options.map{"\($0): \($1)"}.joined(separator: "\n").replacingOccurrences(of: "\n", with: "\n\t"))")
-        }
-        let codeFormatter: CodeFormatter
-        if let formatter = templateConfig.formatter {
-
-            switch formatter {
-            case "swift":
-                codeFormatter = SwiftFormatter(spec: spec)
-            default:
-                codeFormatter = CodeFormatter(spec: spec)
-                writeMessage("Unrecognized formatter \(formatter). Using default")
-                return
-            }
-        }
-        else {
+        switch formatter {
+        case "swift":
+            codeFormatter = SwiftFormatter(spec: spec)
+        default:
             codeFormatter = CodeFormatter(spec: spec)
+            writeMessage("Unrecognized formatter \(formatter). Using default")
+            return
         }
+    }
+    else {
+        codeFormatter = CodeFormatter(spec: spec)
+    }
 
-        let context = codeFormatter.getContext()
+    let context = codeFormatter.getContext()
 
-        let codegen = Codegen(context: context, destination: destinationPath, templateConfig: templateConfig)
+    let codegen = Codegen(context: context, destination: destinationPath, templateConfig: templateConfig)
 
-        writeMessage("Destination: \(destinationPath.absolute())")
-        if clean {
-            try? destinationPath.delete()
-            writeMessage("Cleaned destination")
-        }
+    writeMessage("Destination: \(destinationPath.absolute())")
 
+    if clean {
+        try? destinationPath.delete()
+        writeMessage("Cleaned destination")
+    }
+    do {
         try codegen.generate()
     } catch let error {
-        writeError("Error: \(error.localizedDescription)")
+        writeError("Error generating code: \(error)")
+        exit(EXIT_FAILURE)
     }
+}
+
+func getCountString(counts: [String:Int]) -> String {
+    return counts.map{"\($0.value) \($0.value == 1 ? $0.key : "\($0.key)s")"}.joined(separator: ", ")
 }
 
 func isReadable(path: Path) -> Path {

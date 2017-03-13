82a559cdbd8b09a4d6358e32b6e85cf2ce158b6d
Yonas Kolb <yonaskolb@gmail.com>
2017-03-13 19:36:31 +0700

    update PathKit and Stencil

--- Package.swift
+++ Package.swift
@@ -7,10 +7,10 @@ let package = Package(
         Target(name: "SwagGenKit", dependencies: ["Swagger"]),
     ],
     dependencies: [
-        .Package(url: "https://github.com/kylef/PathKit.git", Version(0, 7, 0)),
-        .Package(url: "https://github.com/kylef/Commander.git", Version(0, 6, 0)),
-        .Package(url: "https://github.com/yonaskolb/Stencil.git", Version(0, 7, 3)),
-        .Package(url: "https://github.com/jpsim/Yams.git", Version(0, 2, 0)),
-        .Package(url: "https://github.com/lucianomarisi/JSONUtilities.git", Version(3, 2, 0)),
+        .Package(url: "https://github.com/kylef/PathKit.git", majorVersion: 0, minor: 8),
+        .Package(url: "https://github.com/kylef/Commander.git", majorVersion: 0, minor: 6),
+        .Package(url: "https://github.com/yonaskolb/Stencil.git", majorVersion: 0, minor: 8),
+        .Package(url: "https://github.com/jpsim/Yams.git", majorVersion: 0, minor: 2),
+        .Package(url: "https://github.com/lucianomarisi/JSONUtilities.git", majorVersion: 3, minor: 2),
     ]
 )

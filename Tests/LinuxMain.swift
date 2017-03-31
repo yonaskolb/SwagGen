a1f6fb7ac1222a4bf0cc3f6c4e2f87c32cc4f5bc
Yonas Kolb <yonaskolb@gmail.com>
2017-03-31 16:03:40 +0700

    swagGenKit: stub tests

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -2,3 +2,4 @@ import SwaggerTests
 import SwagKenKitTests
 
 swaggerTests()
+swagGenKitTests()
---
+++ Tests/SwagGenKitTests/XCTest.swift
@@ -0,0 +1,7 @@
+import XCTest
+
+class SwagGenTests: XCTestCase {
+    func testRunSwagGenTests() {
+
+    }
+}

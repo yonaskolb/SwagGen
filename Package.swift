0d5c79d30bb8f01879834902208a4d9b5249586c
Yonas Kolb <yonaskolb@gmail.com>
2017-03-31 15:29:31 +0700

    package: add Spectre

--- Package.swift
+++ Package.swift
@@ -12,5 +12,6 @@ let package = Package(
         .Package(url: "https://github.com/yonaskolb/Stencil.git", majorVersion: 0, minor: 8),
         .Package(url: "https://github.com/jpsim/Yams.git", majorVersion: 0, minor: 2),
         .Package(url: "https://github.com/lucianomarisi/JSONUtilities.git", majorVersion: 3, minor: 2),
+        .Package(url: "https://github.com/kylef/Spectre.git", majorVersion: 0, minor: 7),
     ]
 )

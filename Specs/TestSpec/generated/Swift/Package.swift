f5c12b2f8e625dfedc2123e53782e36e466f8f62
Yonas Kolb <yonaskolb@gmail.com>
2017-07-09 18:06:36 +0200

    add test spec

---
+++ Specs/TestSpec/generated/Swift/Package.swift
@@ -0,0 +1,10 @@
+import PackageDescription
+
+let package = Package(
+    name: "TestSpec",
+    dependencies: [
+        .Package(url: "https://github.com/lucianomarisi/JSONUtilities.git", "3.2.0"),
+        .Package(url: "https://github.com/Alamofire/Alamofire.git", "4.4.0"),
+        .Package(url: "https://github.com/antitypical/Result.git", "3.2.1"),
+    ]
+)

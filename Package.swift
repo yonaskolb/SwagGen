44dae4f4c7a94b536207f660e708ac719d791140
Yonas Kolb <yonaskolb@gmail.com>
2017-07-29 16:12:57 +0200

    upgrade Yams

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
         .Package(url: "https://github.com/kylef/PathKit.git", majorVersion: 0, minor: 8),
         .Package(url: "https://github.com/kylef/Commander.git", majorVersion: 0, minor: 6),
         .Package(url: "https://github.com/yonaskolb/Stencil.git", majorVersion: 0, minor: 9),
-        .Package(url: "https://github.com/jpsim/Yams.git", majorVersion: 0, minor: 2),
+        .Package(url: "https://github.com/jpsim/Yams.git", majorVersion: 0, minor: 3),
         .Package(url: "https://github.com/yonaskolb/JSONUtilities.git", majorVersion: 3, minor: 3),
         .Package(url: "https://github.com/kylef/Spectre.git", majorVersion: 0, minor: 7),
         .Package(url: "https://github.com/onevcat/Rainbow", majorVersion: 2),

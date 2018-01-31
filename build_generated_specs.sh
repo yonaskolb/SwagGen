78988daa25891b7806315b3fa86c3e61bfd599a0
Yonas Kolb <yonaskolb@gmail.com>
2018-01-31 14:47:21 +1100

    improve build_generated_specs output

--- build_generated_specs.sh
+++ build_generated_specs.sh
@@ -1,6 +1,7 @@
 for spec in Specs/**
 do
-    echo Building ${spec}
+    echo ""
+    echo "📦  Building ${spec}"
     swift build --package-path ${spec}/generated/Swift --build-path Specs/.build -c release
     rm ${spec}/generated/Swift/Package.resolved
 done

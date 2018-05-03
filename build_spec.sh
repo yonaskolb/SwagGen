2ce9bdada24609450a317f68afaff34f4a2468e3
Yonas Kolb <yonaskolb@gmail.com>
2018-05-04 03:45:54 +1000

    remove test Package.resolve

--- build_spec.sh
+++ build_spec.sh
@@ -12,9 +12,12 @@ else
 	SWAGGER_SPEC=$SPEC_PATH/spec.json
 fi
 
+rm ${SPEC_PATH}/generated/Swift/Package.resolved
+
 echo "📦  Generating $SPEC_PATH"
 swift run SwagGen --template Templates/Swift/template.yml --spec ${SWAGGER_SPEC} --destination $SPEC_PATH/generated/Swift --option name:$SPEC_NAME --clean all
 
 echo "📦  Building $SPEC_PATH"
 swift build --package-path ${SPEC_PATH}/generated/Swift --build-path Specs/.build -c release
+
 rm ${SPEC_PATH}/generated/Swift/Package.resolved

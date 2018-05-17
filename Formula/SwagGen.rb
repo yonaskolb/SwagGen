9662baee30f3250a1ef74e60396872a5a7a437ef
Yonas Kolb <yonaskolb@gmail.com>
2018-05-17 17:10:27 +1000

    Update brew to 2.1.0

--- Formula/SwagGen.rb
+++ Formula/SwagGen.rb
@@ -1,8 +1,8 @@
 class Swaggen < Formula
   desc "Swagger/OpenAPISpec code generator written in Swift"
   homepage "https://github.com/yonaskolb/SwagGen"
-  url "https://github.com/yonaskolb/SwagGen/archive/2.0.0.tar.gz"
-  sha256 "ff307102cbb8e864b6c0d2c5b7bc7f4ba2859919d472b70c6117f4552836aebe"
+  url "https://github.com/yonaskolb/SwagGen/archive/2.1.0.tar.gz"
+  sha256 "9de0c1bb38366d806f2328d75db16aa253512bac840dda2605d36d8784bdb253"
   head "https://github.com/yonaskolb/SwagGen.git"
 
   depends_on :xcode

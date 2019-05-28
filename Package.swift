e5d457a8dfa76254b1657ccdcb4175ffb6651e11
leo.wang <leo.wang@massive.co>
2019-05-28 14:31:59 +1000

    fix conflicting requirements in dependencies

--- Package.swift
+++ Package.swift
@@ -11,11 +11,11 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/kylef/PathKit.git", from: "0.8.0"),
         .package(url: "https://github.com/jakeheis/SwiftCLI", from: "5.0.0"),
-        .package(url: "https://github.com/yonaskolb/Stencil.git", from: "0.9.0"),
+        .package(url: "https://github.com/stencilproject/Stencil.git", from: "0.13.0"),
         .package(url: "https://github.com/jpsim/Yams.git", from: "0.5.0"),
-        .package(url: "https://github.com/yonaskolb/JSONUtilities.git", from: "3.3.0"),
+        .package(url: "https://github.com/yonaskolb/JSONUtilities.git", from: "4.1.0"),
         .package(url: "https://github.com/kylef/Spectre.git", from: "0.7.0"),
-        .package(url: "https://github.com/onevcat/Rainbow.git", from: "2.1.0"),
+        .package(url: "https://github.com/onevcat/Rainbow.git", from: "3.1.0"),
     ],
     targets: [
         .target(name: "SwagGen", dependencies: [

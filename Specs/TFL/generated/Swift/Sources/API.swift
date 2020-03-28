e274b0dfca06ff95203cd53d63a6433d63f197d4
Florian Schliep <florian@appiculous.com>
2020-03-28 16:18:55 +0100

    Update fixtures

--- Specs/TFL/generated/Swift/Sources/API.swift
+++ Specs/TFL/generated/Swift/Sources/API.swift
@@ -14,7 +14,9 @@ public struct TFL {
     public static var safeArrayDecoding = false
 
     /// Used to encode Dates when uses as string params
-    public static var dateEncodingFormatter = DateFormatter(formatString: "yyyy-MM-dd'T'HH:mm:ssZZZZZ")
+    public static var dateEncodingFormatter = DateFormatter(formatString: "yyyy-MM-dd'T'HH:mm:ssZZZZZ",
+                                                            locale: Locale(identifier: "en_US_POSIX"),
+                                                            calendar: Calendar(identifier: .gregorian))
 
     public static let version = "v1"
 

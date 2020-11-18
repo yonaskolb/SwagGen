7554495a57b37aaebf6abf6f08096c14a5a47147
Liquidsoul <liquidsoul@liquidsoul.fr>
2020-11-18 11:06:50 +0100

    Add public init to NumberSchema

--- Sources/Swagger/Schema/NumberSchema.swift
+++ Sources/Swagger/Schema/NumberSchema.swift
@@ -7,6 +7,20 @@ public struct NumberSchema {
     public let minimum: Double?
     public let exclusiveMinimum: Double?
     public let multipleOf: Double?
+
+    public init(format: NumberFormat? = nil,
+                minimum: Double? = nil,
+                exclusiveMinimum: Double? = nil,
+                maximum: Double? = nil,
+                exclusiveMaximum: Double? = nil,
+                multipleOf: Double? = nil) {
+        self.format = format
+        self.minimum = minimum
+        self.exclusiveMinimum = exclusiveMinimum
+        self.maximum = maximum
+        self.exclusiveMaximum = exclusiveMaximum
+        self.multipleOf = multipleOf
+    }
 }
 
 public enum NumberFormat: String {

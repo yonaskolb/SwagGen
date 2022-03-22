500bbe93670828ce5bbbb4de34848fbd45c8a4b9
dankinsoid <voidilov@gmail.com>
2022-03-22 15:39:05 +0300

    4.7

--- Formula/SwagGen.rb
+++ Formula/SwagGen.rb
@@ -1,7 +1,7 @@
 class Swaggen < Formula
   desc "Swagger/OpenAPISpec code generator written in Swift"
   homepage "https://github.com/dankinsoid/SwagGen"
-  url "https://github.com/dankinsoid/SwagGen/archive/4.6.0.tar.gz"
+  url "https://github.com/dankinsoid/SwagGen/archive/4.7.0.tar.gz"
   sha256 "78afc0750b56a3d76d89960c8dd2c8740d10f0c76d75ac1b8d7ad63426dae91d"
   head "https://github.com/dankinsoid/SwagGen.git"
 
